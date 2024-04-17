7ec32c502af135b9df76c4d3b21aaed36deaf59f
JVFelipe <juva292@vidalibarraquer.net>
2024-04-17 09:28:01 +0200

    m8

--- demo/src/main/java/com/example/Guest.java
+++ demo/src/main/java/com/example/Guest.java
@@ -1,5 +1,24 @@
 package com.example;
 
+/**
+ * @author Juan Felipe Vallejo Quintero
+ * Guest class which extends Person and represents a guest
+ */
 public class Guest extends Person {
     String nationality;
+
+    /**
+     * "Getter" to retrieve the nationatility of the guest
+     * @return the nationality of the guest
+     */
+    public String getNationality() {
+        return nationality;
+    }
+    /**
+     * "Setter" to set the nationality of the guest
+     * @param nationality the new nationality of the guest
+     */
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
 }
--- demo/src/main/java/com/example/Manager.java
+++ demo/src/main/java/com/example/Manager.java
@@ -1,8 +1,39 @@
 package com.example;
 
 import java.util.Date;
-
+/**
+ * @author Juan Felipe Vallejo Quintero
+ * Manager class which extends Person and represents a manager
+ */
 public class Manager extends Person {
     Date startDate;
     int experience;
+    /**
+     * "Getter" to retrieve the start date of the manager's tenure
+     * @return the start date of the manager's tenure
+     */
+    public Date getStartDate() {
+        return startDate;
+    }
+    /**
+     * "Setter" to set the start date of the manager's tenure
+     * @param startDate the new start date of the manager's tenure
+     */
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+    /**
+     * "Getter" to retrieve the experience level of the manager
+     * @return the experience level of the manager
+     */
+    public int getExperience() {
+        return experience;
+    }
+    /**
+     * "Setter" to set the experience level of the manager
+     * @param experience the new experience level of the manager
+     */
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
 }
